fb7de4198e8959f500063a5af8a01e9cfdf056aa
Joseph Cameron <jf.cameron@gmail.com>
2018-05-15 22:37:06 -0400

    corrected emscripten symbol

--- workspace/build.sh
+++ workspace/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git clean -fxd;
 cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" \
-      -DCMAKE_TOOLCHAIN_FILE=${EMSCRIPTEN}/incoming/cmake/Modules/Platform/Emscripten.cmake \
+      -DCMAKE_TOOLCHAIN_FILE=${EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake \
       .. && \
 make
